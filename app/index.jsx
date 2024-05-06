55a8538a5cbf08cbd7f4c5dd178cf05f2792520f
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-05-06 17:07:58 +1000

    Finish onboarding screen

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,13 @@ import { ScrollView, Image, View, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { images } from "../constants"
 import CustomButton from "../components/custom_button"
+import { Redirect, router } from "expo-router"
 
 export default function App() {
     return (
         <SafeAreaView className='bg-primary h-full'>
             <ScrollView contentContainerStyle={{ height: "100%" }}>
-                <View className='w-full justify-center items-center h-full px-4'>
+                <View className='w-full justify-center items-center min-h-[85vh] px-4'>
                     <Image
                         source={images.logo}
                         className='w-[130px] h-[84px]'
@@ -42,7 +43,7 @@ export default function App() {
 
                     <CustomButton
                         title='Continue with Email'
-                        handlePress={() => {}}
+                        handlePress={() => router.push("/sign-in")}
                         containerStyles='w-full mt-7'
                     />
                 </View>
