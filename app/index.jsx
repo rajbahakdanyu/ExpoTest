61df610b6bb8a824fff8ddb2d15bbd6a5145c557
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-05-06 16:42:58 +1000

    Added bottom tab bar

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar"
 import { Text, View } from "react-native"
+import { Link } from "expo-router"
 
 export default function App() {
     return (
-        <View>
+        <View className='flex-1 justify-center items-center bg-white'>
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style='auto' />
+            <Link href='/home'>Go to home</Link>
         </View>
     )
 }
