f8ecd7d989f5a5a115ee59f7fb73cb01479ca4ec
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2024-05-12 17:28:45 +1000

    Finish

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,34 +1,52 @@
-import React, { useEffect } from "react"
-import { SplashScreen, Stack } from "expo-router"
-import { useFonts } from "expo-font"
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
 
-SplashScreen.preventAutoHideAsync()
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-    const [fontsLoaded, error] = useFonts({
-        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
-    })
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
 
-    useEffect(() => {
-        if (error) throw error
-        if (fontsLoaded) SplashScreen.hideAsync()
-    }, [fontsLoaded, error])
+  useEffect(() => {
+    if (error) throw error;
 
-    if (!fontsLoaded && !error) return null
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
 
-    return (
-        <Stack>
-            <Stack.Screen name='index' options={{ headerShown: false }} />
-        </Stack>
-    )
-}
+  if (!fontsLoaded) {
+    return null;
+  }
 
-export default RootLayout
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
+  );
+};
+
+export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,55 +1,69 @@
-import { StatusBar } from "expo-status-bar"
-import { ScrollView, Image, View, Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
-import { images } from "../constants"
-import CustomButton from "../components/custom_button"
-import { Redirect, router } from "expo-router"
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
-    return (
-        <SafeAreaView className='bg-primary h-full'>
-            <ScrollView contentContainerStyle={{ height: "100%" }}>
-                <View className='w-full justify-center items-center min-h-[85vh] px-4'>
-                    <Image
-                        source={images.logo}
-                        className='w-[130px] h-[84px]'
-                        resizeMode='contain'
-                    />
+import { images } from "../constants";
+import { CustomButton, Loader } from "../components";
+import { useGlobalContext } from "../context/GlobalProvider";
 
-                    <Image
-                        source={images.cards}
-                        className='max-w-[380px] w-full h-[300px]'
-                        resizeMode='contain'
-                    />
+const Welcome = () => {
+  const { loading, isLogged } = useGlobalContext();
 
-                    <View className='reletive mt-5'>
-                        <Text className='text-3xl text-white font-bold text-center'>
-                            Discover Endless{"\n"}
-                            Possibilities with{" "}
-                            <Text className='text-secondary-200'>Aora</Text>
-                        </Text>
+  if (!loading && isLogged) return <Redirect href="/home" />;
 
-                        <Image
-                            source={images.path}
-                            className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
-                            resizeMode='contain'
-                        />
-                    </View>
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <Loader isLoading={loading} />
 
-                    <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
-                        Where Creativity Meets Innovation: Embark on a Journey
-                        of Limitless Exploration with Aora
-                    </Text>
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
 
-                    <CustomButton
-                        title='Continue with Email'
-                        handlePress={() => router.push("/sign-in")}
-                        containerStyles='w-full mt-7'
-                    />
-                </View>
-            </ScrollView>
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
 
-            <StatusBar backgroundColor='#161622' style='light' />
-        </SafeAreaView>
-    )
-}
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
